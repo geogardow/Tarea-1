3c0d3d99f82a8c81888ec30746cdfe07ca1ae1ac
geogardow <85602062+geogardow@users.noreply.github.com>
2021-08-21 02:56:22 -0600

    socket
    Se ha creado el socket, todavía no lee el texto del botón, sólo es la maqueta

--- src/main/java/com/example/tarea1sockets/HelloApplication.java
+++ src/main/java/com/example/tarea1sockets/HelloApplication.java
@@ -1,9 +1,15 @@
 package com.example.tarea1sockets;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.DataOutputStream;
+import java.awt.event.*;
+import java.net.*;
+
 
 import java.io.IOException;
 
@@ -11,7 +17,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,5 +25,19 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+    private class sendData implements ActionListener {
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            try {
+                Socket socketClient = new Socket("192.168.0.4", 9999);
+                DataOutputStream infoStream = new DataOutputStream(socketClient.getOutputStream());
+                infoStream.writeUTF("PRUEBA");
+                infoStream.close();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tarea1sockets {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.tarea1sockets to javafx.fxml;
