aef3f7fdfb05e67cecb4ad8b472c732fa6216454
geogardow <85602062+geogardow@users.noreply.github.com>
2021-08-21 03:39:50 -0600

    Interfaz
    Movimiento, transparencia, detalles y asociación de imagen a cierre de app

--- src/main/java/com/example/tarea1sockets/HelloApplication.java
+++ src/main/java/com/example/tarea1sockets/HelloApplication.java
@@ -2,9 +2,13 @@ package com.example.tarea1sockets;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.DataOutputStream;
 import java.awt.event.*;
@@ -14,11 +18,28 @@ import java.net.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
+
+    private double xOffset;
+    private double yOffset;
+
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent mouseEvent) {
+                xOffset= mouseEvent.getSceneX();
+                yOffset= mouseEvent.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent mouseEvent) {
+                stage.setX(mouseEvent.getScreenX()-xOffset);
+                stage.setY(mouseEvent.getScreenY()-yOffset);
+            }
+        });
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setTitle("Tarea 1");
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/tarea1sockets/HelloController.java
+++ src/main/java/com/example/tarea1sockets/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.tarea1sockets;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void exitButton (MouseEvent mouseEvent){
+        Platform.exit();
+        System.exit(0);
     }
+
 }
